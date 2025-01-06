cd48a85b3039ddd399990ff00e91a5be54e4d0a9
Harshita2815 <harshitaprakash2815@gmail.com>
2025-01-06 19:48:18 +0530

    csv file

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from src.exception import CustomException
 import os 
 import sys
 from src.logger import logging
-def test():
-    try:
-        logging.info("error of division by 0")
-        a=1/0
-    except Exception as e:
-        raise CustomException(e,sys)
+# def test():
+#     try:
+#         logging.info("error of division by 0")
+#         a=1/0
+#     except Exception as e:
+#         raise CustomException(e,sys)
 
 
 
 
 if __name__ == "__main__":
+    file_path="/Users/harshitaprakash/Desktop/Machine_learning/aps_failure_training_set1.csv"
+    database_name=
 
-    #the above line sets the module
-    #prevents excecution on import
-    try:
-        test()
-    except Exception as e:
-        print(e)
+#     #the above line sets the module
+#     #prevents excecution on import
+#     try:
+#         test()
+#     except Exception as e:
+#         print(e)
 
 
