110c09e1857a1b6bdda288fea0bb296dfef05480
Harshita2815 <harshitaprakash2815@gmail.com>
2025-01-16 18:54:01 +0530

    changing

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
+
+
+
+from src.configuration.mongo_db_connection import MongoDBClient
 from src.exception import CustomException
-import os 
-import sys
+import os , sys
 from src.logger import logging
-# def test():
+#from  sensor.utils import dump_csv_file_to_mongodb_collecton
+#from sensor.entity.config_entity  import TrainingPipelineConfig,DataIngestionConfig
+
+from src.pipeline.training_pipeline import TrainPipeline
+
+# def test_exception():
 #     try:
-#         logging.info("error of division by 0")
+#         logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
 #         a=1/0
 #     except Exception as e:
-#         raise CustomException(e,sys)
-
+#        raise SensorException(e,sys) 
 
 
 
 if __name__ == "__main__":
-    file_path="/Users/harshitaprakash/Desktop/Machine_learning/aps_failure_training_set1.csv"
-    database_name=
 
-#     #the above line sets the module
-#     #prevents excecution on import
-#     try:
-#         test()
-#     except Exception as e:
-#         print(e)
+    # file_path="/Users/myhome/Downloads/sensorlive/aps_failure_training_set1.csv"
+    # database_name="ineuron"
+    # collection_name ="sensor"
+    # dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
 
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
 
